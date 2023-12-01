9f553d915282b55760b81ebde313e670491ae5df
Arthur <beauiv@users.noreply.github.com>
2023-12-01 10:32:17 -0500

    more debugging

--- adventofcode_Dec1/dec01.go
+++ adventofcode_Dec1/dec01.go
@@ -30,13 +30,13 @@ func main() {
 	for scanner.Scan() {
 		numStr := ""
 		convString:=scanner.Text();
-		for i, number := range textNumbers {
-			fmt.Println("Old String: ", convString)
+		fmt.Println("Old String: ", convString)
+		for i, number := range textNumbers {	
 			convString = strings.Replace(convString, number,strconv.Itoa(i),-1)
-			fmt.Println("New String: ", convString)
 		}
+		fmt.Println("New String: ", convString)
 		cleanStr := clearString(convString)
-		fmt.Println(cleanStr)
+		fmt.Println("Clean String: ", cleanStr)
 		for i := 0; i < len(cleanStr); i++ {
 			if i == 0 || i == len(cleanStr)-1 {
 				numStr = numStr + string(cleanStr[i])
